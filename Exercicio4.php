b55d9b722d0a184e97f3af0f5caa90023d5a54f2
MatheusCatapan <m.oliveira.mo355@gmail.com>
2025-05-29 11:27:43 -0300

    varios problemas resolver

---
+++ Exercicio4.php
@@ -0,0 +1,62 @@
+<?php 
+
+    class Pessoa {
+        public $nome;
+        public $endereco;
+        public $email;
+        public $telefone;
+    
+        public function __construct($nome, $endereco, $email, $telefone) {
+            $this->nome = $nome;
+            $this->endereco = $endereco;
+            $this->email = $email;
+            $this->telefone = $telefone;
+        }
+    
+    }
+
+    class Livros {
+        public $nome;
+        public $autor;
+        public $nPaginas;
+        public $disponibilidade;
+        public $disponivel;
+        public Pessoa $alugadoPor;
+
+        public function __construct ($nome, $autor, $nPaginas, $dispinibilidades) {
+            $this->nome = $nome;
+            $this->autor = $autor;
+            $this->nPaginas = $nPaginas;
+            $this->disponibilidade = $dispinibilidades;
+            $this->disponivel = true;
+        }
+        public function alugar(Pessoa $pessoa) {
+            if ($this->disponibilidade) {
+                $this->disponibilidade = false;
+                echo "Livro '{$this->nome}' alugado para {$pessoa->nome}.\n";
+            } else {
+                echo "Livro '{$this->nome}' não está disponível.\n";
+            }
+
+        }
+    
+        public function devolver() {
+            if (!$this->disponivel && $this->alugadoPor !== null) {
+                echo "Livro '{$this->nome}' devolvido por {$this->alugadoPor->nome}.\n";
+                $this->disponivel = true;
+                $this->alugadoPor = null;
+            } else {
+                echo "Livro '{$this->nome}' já está disponível.\n";
+            }
+        }
+
+    }
+
+    $matheus = new Pessoa ("Matheus", "Rua das Rosas 9", "m.oliveira.mo355@gmail.com", "42988634181");
+    
+    $livro1 = new Livros ("Teste", "tigres" , 1500, false);
+    $livro2 = new Livros ("Testing", "ursos" , 1500, true);
+    
+    
+    $livro1->alugar($matheus);
+    $livro1->devolver();
